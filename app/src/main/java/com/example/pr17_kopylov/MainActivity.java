5176ad323ee0e0e01c33b501423aca43deaa2864
MAPAKAC123 <Kopylovmaxim999@gmail.com>
2023-10-01 15:29:09 +0700

    1

--- app/src/main/java/com/example/pr17_kopylov/MainActivity.java
+++ app/src/main/java/com/example/pr17_kopylov/MainActivity.java
@@ -12,100 +12,174 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends Activity implements OnClickListener {
 
     final String LOG_TAG = "myLogs";
 
-    Button btnAdd, btnRead, btnClear;
-    EditText etName, etEmail;
+    String name[] = {"Китай", "Сша", "Бразилия", "Россия", "Япония", "Германия", "Египет", "Италия", "Франция", "Канада",};
+    int people[] = { 1400, 311, 195, 142, 128, 82, 80, 60, 66, 35 };
+    String region[] = { "Азия", "Северная Америка", "Южная Америка", "Европа", "Азия",
+            "Европа", "Африка", "Европа", "Европа", "Северная Америка" };
+
+    Button btnAll, btnFunc, btnPeople, btnSort, btnGroup, btnHaving;
+    EditText etFunc, etPeople, etRegionPeople;
+    RadioGroup rgSort;
+    SQLiteDatabase db;
 
     DBHelper dbHelper;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(this);
+        btnAll = (Button) findViewById(R.id.btnAll);
+        btnAll.setOnClickListener(this);
 
-        btnRead = (Button) findViewById(R.id.btnRead);
-        btnRead.setOnClickListener(this);
+        btnFunc = (Button) findViewById(R.id.btnFunc);
+        btnFunc.setOnClickListener(this);
 
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnClear.setOnClickListener(this);
+        btnPeople = (Button) findViewById(R.id.btnPeople);
+        btnPeople.setOnClickListener(this);
 
-        etName = (EditText) findViewById(R.id.etName);
-        etEmail = (EditText) findViewById(R.id.etEmail);
+        btnSort = (Button) findViewById(R.id.btnSort);
+        btnSort.setOnClickListener(this);
+
+        btnGroup = (Button) findViewById(R.id.btnGroup);
+        btnGroup.setOnClickListener(this);
+
+        btnHaving = (Button) findViewById(R.id.btnHaving);
+        btnHaving.setOnClickListener(this);
+
+        etFunc = (EditText) findViewById(R.id.etFunc);
+        etPeople = (EditText) findViewById(R.id.etPeople);
+        etRegionPeople = (EditText) findViewById(R.id.etRegionPeople);
+
+        rgSort = (RadioGroup) findViewById(R.id.rgSort);
+
 
         // создаем объект для создания и управления версиями БД
         dbHelper = new DBHelper(this);
+
+        db = dbHelper.getWritableDatabase();
+        // проверка существования записей
+        Cursor c = db.query("mytable", null, null, null, null, null, null);
+        if (c.getCount() == 0) {
+            ContentValues cv = new ContentValues();
+            // заполнимтаблицу
+            for (int i = 0; i< 10; i++) {
+                cv.put("name", name[i]);
+                cv.put("people", people[i]);
+                cv.put("region", region[i]);
+                Log.d(LOG_TAG, "id = " + db.insert("mytable", null, cv));
+            }
+        }
+        c.close();
+        dbHelper.close();
+        // эмулируемнажатиекнопкиbtnAll
+        onClick(btnAll);
     }
 
 
     @Override
-    public void onClick(View v) {
-
-        // создаем объект для данных
-        ContentValues cv = new ContentValues();
-
-        // получаем данные из полей ввода
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-
-        // подключаемся к БД
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-
+    public void onClick(View v)
+    {
+        // подключаемсякбазе
+        db = dbHelper.getWritableDatabase();
+        // данныесэкрана
+        String sFunc = etFunc.getText().toString();
+        String sPeople = etPeople.getText().toString();
+        String sRegionPeople = etRegionPeople.getText().toString();
+        // переменныедля query
+        String[] columns = null;
+        String selection = null;
+        String[] selectionArgs = null;
+        String groupBy = null;
+        String having = null;
+        String orderBy = null;
+        // курсор
+        Cursor c = null;
+        // определяем нажатую кнопку
         switch (v.getId()) {
-            case R.id.btnAdd:
-                Log.d(LOG_TAG, "--- Insert in mytable: ---");
-                // подготовим данные для вставки в виде пар: наименование столбца - значение
-
-                cv.put("name", name);
-                cv.put("email", email);
-                // вставляем запись и получаем ее ID
-                long rowID = db.insert("mytable", null, cv);
-                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+            // Все записи
+            case R.id.btnAll:
+            Log.d(LOG_TAG, "--- Все записи ---");
+            c = db.query("mytable", null, null, null, null, null, null);
+            break;
+            // Функция
+            case R.id.btnFunc:
+                Log.d(LOG_TAG, "--- Функция " + sFunc + " ---");
+                columns = new String[] { sFunc };
+                c = db.query("mytable", columns, null, null, null, null, null);
                 break;
-            case R.id.btnRead:
-                Log.d(LOG_TAG, "--- Rows in mytable: ---");
-                // делаем запрос всех данных из таблицы mytable, получаем Cursor
-                Cursor c = db.query("mytable", null, null, null, null, null, null);
-
-                // ставим позицию курсора на первую строку выборки
-                // если в выборке нет строк, вернется false
-                if (c.moveToFirst()) {
-
-                    // определяем номера столбцов по имени в выборке
-                    int idColIndex = c.getColumnIndex("id");
-                    int nameColIndex = c.getColumnIndex("name");
-                    int emailColIndex = c.getColumnIndex("email");
-
-                    do {
-                        // получаем значения по номерам столбцов и пишем все в лог
-                        Log.d(LOG_TAG,
-                                "ID = " + c.getInt(idColIndex) +
-                                        ", name = " + c.getString(nameColIndex) +
-                                        ", email = " + c.getString(emailColIndex));
-                        // переход на следующую строку
-                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
-                    } while (c.moveToNext());
-                } else
-                    Log.d(LOG_TAG, "0 rows");
-                c.close();
+            // Население больше, чем
+            case R.id.btnPeople:
+            Log.d(LOG_TAG, "--- Население больше " + sPeople + " ---");
+            selection = "people > ?";
+            selectionArgs = new String[] { sPeople };
+            c = db.query("mytable", null, selection, selectionArgs, null, null,
+                    null);
+            break;
+            // Населениепорегиону
+            case R.id.btnGroup:
+                Log.d(LOG_TAG, "--- Население по региону ---");
+                columns = new String[] { "region", "sum(people) as people" };
+                groupBy = "region";
+                c = db.query("mytable", columns, null, null, groupBy, null, null);
                 break;
-            case R.id.btnClear:
-                Log.d(LOG_TAG, "--- Clear mytable: ---");
-                // удаляем все записи
-                int clearCount = db.delete("mytable", null, null);
-                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+            // Население по региону больше чем
+            case R.id.btnHaving:
+            Log.d(LOG_TAG, "--- Регионы с населением больше " + sRegionPeople
+                    + " ---");
+            columns = new String[] { "region", "sum(people) as people" };
+            groupBy = "region";
+            having = "sum(people) > " + sRegionPeople;
+            c = db.query("mytable", columns, null, null, groupBy, having, null);
+            break;
+            // Сортировка
+            case R.id.btnSort:
+                // сортировкапо
+                switch (rgSort.getCheckedRadioButtonId()) {
+// наименование
+                    case R.id.rName:
+                    Log.d(LOG_TAG, "--- Сортировка по наименованию ---");
+                    orderBy = "name";
+                    break;
+                    // население
+                    case R.id.rPeople:
+                        Log.d(LOG_TAG, "--- Сортировка по населению ---");
+                        orderBy = "people";
+                        break;
+                    // регион
+                    case R.id.rRegion:
+                        Log.d(LOG_TAG, "--- Сортировка по региону ---");
+                        orderBy = "region";
+                        break;
+                }
+                c = db.query("mytable", null, null, null, null, null, orderBy);
                 break;
         }
-        // закрываем подключение к БД
+        if (c != null) {
+            if (c.moveToFirst()) {
+                String str;
+                do {
+                    str = "";
+                    for (String cn :c.getColumnNames()) {
+                        str = str.concat(cn + " = "
+                                + c.getString(c.getColumnIndex(cn)) + "; ");
+                    }
+                    Log.d(LOG_TAG, str);
+
+                } while (c.moveToNext());
+            }
+            c.close();
+        } else
+            Log.d(LOG_TAG, "Cursor is null");
+
         dbHelper.close();
+
     }
 
 
@@ -113,18 +187,17 @@ public class MainActivity extends Activity implements OnClickListener {
     class DBHelper extends SQLiteOpenHelper {
 
         public DBHelper(Context context) {
-            // конструктор суперкласса
+            // конструкторсуперкласса
             super(context, "myDB", null, 1);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
             Log.d(LOG_TAG, "--- onCreate database ---");
-            // создаем таблицу с полями
+            // создаемтаблицусполями
             db.execSQL("create table mytable ("
-                    + "id integer primary key autoincrement,"
-                    + "name text,"
-                    + "email text" + ");");
+                    + "id integer primary key autoincrement," + "name text,"
+                    + "people integer," + "region text" + ");");
         }
 
         @Override
